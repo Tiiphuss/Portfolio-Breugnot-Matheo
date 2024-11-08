b6e22522e560099dcb5737324c80d65ae89d4075
Matheo Breugnot <matheo9477@gmail.com>
2024-11-08 15:25:50 +0100

    debut

---
+++ public/datas/projets.js
@@ -0,0 +1,8 @@
+export const Projets = [
+    {
+        "title": "React",
+        "description": "Projet react",
+        "pictures": "",
+        "link": "https://github.com/Tiiphuss/OCP5-React"
+    }
+]
